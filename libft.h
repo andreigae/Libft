157ed832a6a1389a9222f225e512ba047a416c95
Andrei Gae Matei <andrei@macrofunnels.com>
2023-01-24 13:25:20 +0100

    Agregar funciones de la segunda parte

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: agae-mat <agae-mat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 17:09:23 by agae-mat          #+#    #+#             */
-/*   Updated: 2023/01/18 17:48:46 by agae-mat         ###   ########.fr       */
+/*   Updated: 2023/01/24 13:24:53 by agae-mat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+// Primera Parte (Funciones de la libreria)
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -40,5 +41,18 @@ char *ft_strnstr(const char *haystack, const char *needle, size_t len);
 int ft_atoi(const char *str);
 void *ft_calloc(size_t count, size_t size);
 char *ft_strdup(const char *s1);
+
+// Segunda Parte (Funciones de la libreria)
+char *ft_substr(char const *s, unsigned int start,size_t len);
+char *ft_strjoin(char const *s1, char const *s2);
+char *ft_strtrim(char const *s1, char const *set);
+char **ft_split(char const *s, char c);
+char *ft_itoa(int n);
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void ft_striteri(char *s, void (*f)(unsigned int,char*));
+void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
+void ft_putendl_fd(char *s, int fd);
+void ft_putnbr_fd(int n, int fd);
 
 #endif
